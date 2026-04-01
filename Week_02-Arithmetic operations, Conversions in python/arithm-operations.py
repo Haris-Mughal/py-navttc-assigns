49736eef7c83d986ca7091780d8954fc86531153
Haris-Mughal <harismirza3456@gmail.com>
2026-04-01 12:34:42 +0500

    week two assign added

---
+++ Week_02-Arithmetic operations, Conversions in python/arithm-operations.py
@@ -0,0 +1,16 @@
+# Step 1: Define variables
+
+x = 50
+y = 100
+
+# Step 2: Execute arithmatic operations
+sum = x + y
+diff = y - x
+prod = x * y
+quot = x / y
+
+# Step 3: Print the results
+print("Sum: ", sum)
+print("Difference: ", diff)
+print("Product: ", prod)
+print("Quotient: ", quot)
